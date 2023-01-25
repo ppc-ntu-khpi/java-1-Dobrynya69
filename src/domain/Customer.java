e18c774f629812effff550e909da6d23c26b992f
Roman <roma040805@gmail.com>
2023-01-25 17:06:25 +0200

    Done

---
+++ src/domain/Customer.java
@@ -0,0 +1,45 @@
+package domain;
+
+public class Customer {
+    
+    public Customer(){
+        this.ID = 69;
+        this.isNew = false;
+        this.total = 255.5F;
+    }
+    
+    private int ID;
+    private boolean isNew;
+    private float total;
+    
+    public void displayCustomerInfo(){
+        System.out.println("ID: " + this.ID);
+        System.out.println("Customer is new: " + this.isNew);
+        System.out.println("Total sum: " + this.total);
+    }
+    
+    public int getID(){
+        return this.ID;
+    }
+    
+    public void setID(int ID){
+        this.ID = ID;
+    }
+    
+    public boolean getIsNew(){
+        return this.isNew;
+    }
+    
+    public void setIsNew(boolean isNew){
+        this.isNew = isNew;
+    }
+    
+    public float getTotal(){
+        return this.total;
+    }
+    
+    public void setTotal(float total){
+        this.total = total;
+    }
+    
+}
